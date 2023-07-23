56d5c3ec18e431680674eab3a0e33101e526563f
ayeon <jay3916@naver.com>
2023-07-23 17:40:05 +0900

    second-commit

--- App.tsx
+++ App.tsx
@@ -4,8 +4,8 @@ import {StyleSheet,View, Text} from "react-native";
 export default function App(){
   return (
     <View style= {{ flex:1 }} >
-      <View style={{ flex:1, backgroundColor:"yellow" }} ></View>
-      <View style={{ flex : 1,backgroundColor:"orange"}}>
+      <View style={{ flex: 1, backgroundColor:"yellow" }} ></View>
+      <View style={{ flex : 2,backgroundColor:"orange"}}>
         <Text>hello</Text>
       </View>
     </View>
